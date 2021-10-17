b45a47c4c51af40591d8e049200bd69f18cdbd54
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2021-10-17 12:32:37 -0300

    Counting iterations

--- src/Timer.js
+++ src/Timer.js
@@ -1,14 +1,35 @@
 import React from "react"
 
 class Timer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            iterations: 0
+        }
+    }
+
     start = () => {
-        setInterval(this.props.action, 1000)
+        const intervalId = setInterval(this.run, 1000)
+        this.setState({intervalId: intervalId})
+    }
+
+    componentWillUnmount = () => {
+        if (this.state.intervalId) {
+            clearInterval(this.state.intervalId)
+        }
+    }
+
+    run = () => {
+        this.setState({
+            iterations: this.state.iterations + 1
+        })
+        this.props.action()
     }
     
     render() {
-        return (
-            <button onClick={this.start}>Start</button>
-        )
+        return <div>
+            <button onClick={this.start}>Start</button> <label>Iterations: {this.state.iterations}</label>
+        </div>
     }
     
 }
