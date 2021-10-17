9648bf87119b0a471355fbe39958f44811ba6cd1
Douglas de Oliveira Mendes <douglas.mendes@gmail.com>
2021-10-17 17:59:13 -0300

    Ant moving according to rules.

--- src/Ant.js
+++ src/Ant.js
@@ -1,12 +1,67 @@
 class Ant {
-    constructor(props) {
-        this.board = props.board
+    constructor(board) {
+        this.board = board
         this.x = 50
         this.y = 50
+        this.direction = Ant.UP
+    }
+
+    static get UP() {
+        return 1;
+    }
+    static get RIGHT() {
+        return 2;
+    }
+    static get DOWN() {
+        return 3;
+    }
+    static get LEFT() {
+        return 4;
     }
 
     move = () => {
-        this.x++
+        if (this.board[this.x][this.y]) {
+            this.goLeft()
+        } else {
+            this.goRight()
+        }
+        this.board[this.x][this.y] = !this.board[this.x][this.y]
+    }
+
+    goLeft = () => {
+        if (this.direction === Ant.UP) {
+            this.x--
+            this.direction = Ant.LEFT
+        } else if (this.direction === Ant.RIGHT) {
+            this.y--
+            this.direction = Ant.UP
+        } else if (this.direction === Ant.DOWN) {
+            this.x++
+            this.direction = Ant.RIGHT
+        } else if (this.direction === Ant.LEFT) {
+            this.y++
+            this.direction = Ant.DOWN
+        } else {
+            console.log("Unexpected direction: " + this.direction)
+        }
+    }
+
+    goRight = () => {
+        if (this.direction === Ant.UP) {
+            this.x++
+            this.direction = Ant.RIGHT
+        } else if (this.direction === Ant.RIGHT) {
+            this.y++
+            this.direction = Ant.DOWN
+        } else if (this.direction === Ant.DOWN) {
+            this.x--
+            this.direction = Ant.LEFT
+        } else if (this.direction === Ant.LEFT) {
+            this.y--
+            this.direction = Ant.UP
+        } else {
+            console.log("Unexpected direction: " + this.direction)
+        }
     }
 }
 
